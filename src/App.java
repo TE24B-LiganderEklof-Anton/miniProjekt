1792b43f4ea958fc2a6f68b49a6c0a9f3bad4f6b
TE24B-Ligandereklof-Anton <anton.ligandereklof@elev.ga.ntig.se>
2026-09-11 11:10:16 +0200

    har lagt till funkionalitet för att boka och se obokade platser

--- src/App.java
+++ src/App.java
@@ -1,30 +1,50 @@
-static int getInput(int minValue, int maxValue){
-        boolean validAnswer = false;
-        String answerString = "";
-        int answerInt = 0;
-        while (!validAnswer){
-            answerString = IO.readln();
-            try {
-                answerInt = Integer.parseInt(answerString);
-                if (answerInt > maxValue || answerInt < minValue){
-                    IO.println("Svaret måste vara mellan " + Integer.toString(minValue) + " och " + Integer.toString(maxValue) + ". Försök igen:");
-                }
-                else
-                {
-                    validAnswer = true;
-                }
-            } catch (Exception e) {
-                IO.println("Ogiltigt Svar. Svaret måste bestå av endast siffror. Försök igen:");
+
+static int getInput(int minValue, int maxValue) {
+    boolean validAnswer = false;
+    String answerString = "";
+    int answerInt = 0;
+    while (!validAnswer) {
+        answerString = IO.readln();
+        try {
+            answerInt = Integer.parseInt(answerString);
+            if (answerInt > maxValue || answerInt < minValue) {
+                IO.println("Svaret måste vara mellan " + Integer.toString(minValue) + " och "
+                        + Integer.toString(maxValue) + ". Försök igen:");
+            } else {
+                validAnswer = true;
             }
+        } catch (Exception e) {
+            IO.println("Ogiltigt Svar. Svaret måste bestå av endast siffror. Försök igen:");
         }
-        return answerInt;
     }
-public static void main(){
-    
-    String[] options = {"Lägg till bokning", "Skriv ut lediga platser","Beräkna vinst", "Hitta bokning", "Se vuxna/barn", "Se fönsterplatser"};
-    for (int i=0; i<options.length; i++){
-        IO.println(Integer.toString(i+1) + ":" + options[i]);
+    return answerInt;
+}
+
+public static void main() {
+    int[] bookings = new int[20];
+    while (true) {
+
+        String[] options = { "Lägg till bokning", "Skriv ut lediga platser", "Beräkna vinst", "Hitta bokning",
+                "Se vuxna/barn", "Se fönsterplatser" };
+        for (int i = 0; i < options.length; i++) {
+            IO.println(Integer.toString(i + 1) + ":" + options[i]);
+        }
+        IO.println("Val:");
+        int choice = getInput(1, options.length + 1);
+        switch (choice) {
+            case 1:
+                IO.println("Ange plats att boka:");
+                int spot = getInput(1, bookings.length + 1);
+                IO.println("Ange födelsedag (ååååmmdd):");
+                int number = getInput(10000000, 99999999);
+                bookings[spot - 1] = number;
+            case 2:
+                for (int i = 0; i < bookings.length; i++) {
+                    if (bookings[i] == 0) {
+                        IO.println(Integer.toString(i+1));
+                    }
+
+                }
+        }
     }
-    IO.println("Val:");
-    int val = getInput(1, options.length+1);
 }
